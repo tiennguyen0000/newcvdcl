4821c5996816ed28fb513b3a8b66cc0ce65d9e79
TienNguyen000 <160461053+tiennguyen0000@users.noreply.github.com>
2024-11-30 19:18:48 +0700

    up

--- WED/model/call.py
+++ WED/model/call.py
@@ -10,7 +10,7 @@ from WED.model.main import run
 def add_interrupt_attribute(self):
     self.interrupt = False  # Set to False by default
 
-def crdeimg(scal, omega=3, t_exit=15):
+def crdeimg(scal):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model_type = Model_Type.SDXL
     scheduler_type = Scheduler_Type.DDIM
@@ -18,7 +18,7 @@ def crdeimg(scal, omega=3, t_exit=15):
     input_image = base64_to_image(scal.img_base64)
     original_shape = input_image.size
     input_image = input_image.resize((1024, 1024))
-    prompt = scal.promt # 'smile' for "009698.jpg", 'anime' for "Arknight.jpg"
+    prompt = scal.prompt # 'smile' for "009698.jpg", 'anime' for "Arknight.jpg"
 
     config = RunConfig(model_type = model_type,
                         num_inference_steps = 50,
@@ -38,19 +38,22 @@ def crdeimg(scal, omega=3, t_exit=15):
 
     pipe_inference.__class__.interrupt
     rec_image = pipe_inference(image = inv_latent,
-                              prompt = scal.promt_fw,
-                              denoising_start=0.0,
-                              num_inference_steps = config.num_inference_steps,
-                              guidance_scale = 1.0,
-                                omega=omega, # omega=3 for "009698.jpg", omega=5 for "Arknight.jpg"
-                                gamma=3, # gamma=3 for "009698.jpg", gamma=3 for "Arknight.jpg"
-                                inv_latents=all_latents,
-                                prompt_embeds_ref=other_kwargs[0],
-                                added_cond_kwargs_ref=other_kwargs[1],
-                                t_exit=t_exit, # t_exit=15 for "009698.jpg", t_exit=25 for "Arknight.jpg"
-                                ).images[0]
-    
-    return rec_image.resize(original_shape)
+                            prompt = scal.prompt_fw,
+                            denoising_start=0.0,
+                            num_inference_steps = config.num_inference_steps,
+                            guidance_scale = 1.0,
+                            omega=scal.omega, # omega=3 for "009698.jpg", omega=5 for "Arknight.jpg"
+                            gamma=3, # gamma=3 for "009698.jpg", gamma=3 for "Arknight.jpg"
+                            inv_latents=all_latents,
+                            prompt_embeds_ref=other_kwargs[0],
+                            added_cond_kwargs_ref=other_kwargs[1],
+                            edit_threshold = scal.edit_threshold,
+                            edit_guidance_scale = scal.edit_guidance_scale,
+                            reverse_editing_direction = scal.reverse_editing_direction,
+                            t_exit = scal.t_exit, # t_exit=15 for "009698.jpg", t_exit=25 for "Arknight.jpg"
+                            ).images[0]
+
+    return rec_image.resize((1024, int(1024 * original_shape[1] / original_shape[0])))
 
 def genI(txt: TextInput):
     from WED.model.Pipeline.TTI import pipelineT2i, generator
