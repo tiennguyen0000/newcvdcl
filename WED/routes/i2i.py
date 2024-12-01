d250e9b0be40849ecbd548d8d4ac52613716f309
TienNguyen000 <160461053+tiennguyen0000@users.noreply.github.com>
2024-12-02 02:05:06 +0700

    Update i2i.py

--- WED/routes/i2i.py
+++ WED/routes/i2i.py
@@ -6,16 +6,19 @@ from WED.schemas.genI_schemas import tai_rps, TextInput, image_to_base64
 from WED.model.call import crdeimg
 
 router = APIRouter()
-
+device = 'cuda'
+model_type = Model_Type.SDXL
+scheduler_type = Scheduler_Type.DDIM
+pipe_inversion, pipe_inference = get_pipes(model_type, scheduler_typedevice=device)
 @router.post('/increase')
 def predict(scal : tai_rps):
-    response = crdeimg(scal, omega=3)
+    response = crdeimg(scal, pipe_inversion, pipe_inference, omega=3)
 
     return image_to_base64(response)
 
 @router.post('/decrease')
 def predictcx(scal : tai_rps):
-    response = crdeimg(scal, omega=-3)
+    response = crdeimg(scal, pipe_inversion, pipe_inference, omega=-3)
     # image = Image.open('/kaggle/input/dsgssdd/images.jpg')   
     return image_to_base64(response)
     # return 0
