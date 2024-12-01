0a0b5639422d8f12334169a8c35bdb8a7ec9f868
TienNguyen000 <160461053+tiennguyen0000@users.noreply.github.com>
2024-12-01 17:17:58 +0700

    Update call.py

--- WED/model/call.py
+++ WED/model/call.py
@@ -10,7 +10,13 @@ from WED.model.main import run
 def add_interrupt_attribute(self):
     self.interrupt = False  # Set to False by default
 
-def crdeimg(scal):
+def crdeimg(scal,
+            omega = 0,
+            edit_threshold = [0.9],
+            edit_guidance_scale = [10],
+            reverse_editing_direction = [False, False, False],
+            t_exit = 0):
+    
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model_type = Model_Type.SDXL
     scheduler_type = Scheduler_Type.DDIM
@@ -21,8 +27,8 @@ def crdeimg(scal):
     prompt = scal.prompt # 'smile' for "009698.jpg", 'anime' for "Arknight.jpg"
 
     config = RunConfig(model_type = model_type,
-                        num_inference_steps = 50,
-                        num_inversion_steps = 50,
+                        num_inference_steps = 20,
+                        num_inversion_steps = 20,
                         num_renoise_steps = 1,
                         scheduler_type = scheduler_type,
                         perform_noise_correction = False,
@@ -37,22 +43,27 @@ def crdeimg(scal):
 
 
     pipe_inference.__class__.interrupt
-    rec_image = pipe_inference(image = inv_latent,
+    if t_exit == 0:
+        t_exit = scal.num_ts // 3
+    
+    rec_image, mask = pipe_inference(image = inv_latent,
                             prompt = scal.prompt_fw,
                             denoising_start=0.0,
                             num_inference_steps = config.num_inference_steps,
                             guidance_scale = 1.0,
-                            omega=scal.omega, # omega=3 for "009698.jpg", omega=5 for "Arknight.jpg"
+                            omega=omega, # omega=3 for "009698.jpg", omega=5 for "Arknight.jpg"
                             gamma=3, # gamma=3 for "009698.jpg", gamma=3 for "Arknight.jpg"
                             inv_latents=all_latents,
                             prompt_embeds_ref=other_kwargs[0],
                             added_cond_kwargs_ref=other_kwargs[1],
-                            edit_threshold = scal.edit_threshold,
-                            edit_guidance_scale = scal.edit_guidance_scale,
-                            reverse_editing_direction = scal.reverse_editing_direction,
-                            t_exit = scal.t_exit, # t_exit=15 for "009698.jpg", t_exit=25 for "Arknight.jpg"
+                            edit_threshold = edit_threshold,
+                            edit_guidance_scale = edit_guidance_scale,
+                            reverse_editing_direction = reverse_editing_direction,
+                            t_exit = t_exit, # t_exit=15 for "009698.jpg", t_exit=25 for "Arknight.jpg"
                             ).images[0]
-
+    rec_image = rec_image.images[0]
+    mask = mask.images[0]
+    mask.save("../mask.jpg")
     return rec_image.resize((1024, int(1024 * original_shape[1] / original_shape[0])))
 
 def genI(txt: TextInput):
